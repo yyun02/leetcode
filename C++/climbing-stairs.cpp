fc810d74ff24281f335278a9d7ee4f75acce51ff
yyun02 <124532051+yyun02@users.noreply.github.com>
2024-01-04 15:15:43 -0500

    Create climbing-stairs.cpp

---
+++ C++/climbing-stairs.cpp
@@ -0,0 +1,34 @@
+/*
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        if (n == 0 || n ==1 ){
+            return 1;
+        }
+
+         return climbStairs (n-1) + climbStairs (n-2);
+    }
+};
+
+*/
+
+//https://leetcode.com/problems/climbing-stairs/solutions/3708750/4-method-s-beat-s-100-c-java-python-beginner-friendly/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
